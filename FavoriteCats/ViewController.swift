7f1ec8582454dd82da911532d37c46481cfe8470
KsGuseva <kguseva@avanta.co>
2024-03-25 19:24:57 +0200

    Initial Commit

---
+++ FavoriteCats/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FavoriteCats
+//
+//  Created by Kseniya Kovgorenya on 25.03.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
